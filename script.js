69b4a3e09e17514e6d5c36052873c112ba702a0c
Anyistorres <inganyistorresadso@gmail.com>
2026-05-15 18:21:17 -0500

    Segundo commit

---
+++ script.js
@@ -0,0 +1,9 @@
+
+console.log("Formulario Listo");
+
+const formulario = document.getElementById("formulario-contacto");
+
+formulario.addEventListener("submit", ()=> {
+    alert("Mensaje enviado correctamente");
+});
+
